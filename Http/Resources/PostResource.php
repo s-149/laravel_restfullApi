bd43432dee44f97ac34609515ab36bfbb8557da5
Sabar <65281509+s-149@users.noreply.github.com>
2023-04-30 11:40:42 +0700

    up 1
    mengirim folder http dari folder app

---
+++ Http/Resources/PostResource.php
@@ -0,0 +1,61 @@
+<?php
+
+// namespace App\Http\Resources;
+
+// use Illuminate\Http\Request;
+// use Illuminate\Http\Resources\Json\JsonResource;
+
+// class PostResource extends JsonResource
+// {
+//     /**
+//      * Transform the resource into an array.
+//      *
+//      * @return array<string, mixed>
+//      */
+//     public function toArray(Request $request): array
+//     {
+//         return parent::toArray($request);
+//     }
+// }
+
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class PostResource extends JsonResource
+{
+    //define properti
+    public $status;
+    public $message;
+    
+    /**
+     * __construct
+     *
+     * @param  mixed $status
+     * @param  mixed $message
+     * @param  mixed $resource
+     * @return void
+     */
+    public function __construct($status, $message, $resource)
+    {
+        parent::__construct($resource);
+        $this->status  = $status;
+        $this->message = $message;
+    }
+
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array
+     */
+    public function toArray($request)
+    {
+        return [
+            'success'   => $this->status,
+            'message'   => $this->message,
+            'data'      => $this->resource
+        ];
+    }
+}
